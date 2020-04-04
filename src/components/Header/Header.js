d23ad30f9726d2a5158adf60faafceb40973f575
Akbar Hirani <akbar.hirani123@gmail.com>
2020-04-04 16:55:05 -0400

    commit old changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,33 +23,35 @@ class Header extends Component {
     let menuActive = this.state.isToggleOn ? 'is-active' : '';
 
     return (
-        <div className="navbar has-shadow">
-            <div className="navbar-brand">
-                <a className="navbar-item">MyCompany</a>
-                <a className={"navbar-burger burger "+menuActive} onClick={this.handleClick}>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
-            </div>
+        <div className="navbar">
+            <div className="container">
+                <div className="navbar-brand">
+                    <Link to="/"  className="navbar-item">MyCompany</Link>
+                    <a className={"navbar-burger burger "+menuActive} onClick={this.handleClick}>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
 
-            <div className={'navbar-end navbar-menu '+menuActive}>
-                <div className="navbar-start"></div>
-                <div className="navbar-end">    
-                    <Link to="/" className="navbar-item r-item">Home</Link>
-                    <Link to="/faq" className="navbar-item r-item">Features</Link>
-                    <Link to="/faq" className="navbar-item r-item">About</Link>
-                    <Link to="/faq" className="navbar-item r-item">FAQ</Link>
-                
-                    <div className="navbar-item">
-                        <p className="control">
-                            <a className="button is-primary is-outlined">
-                                <span className="icon">
-                                    <i className="fa fa-download"></i>
-                                </span>
-                                <span>Join Now</span>
-                            </a>
-                        </p>
+                <div className={'navbar-end navbar-menu '+menuActive}>
+                    <div className="navbar-start"></div>
+                    <div className="navbar-end">    
+                        <Link to="/" className="navbar-item r-item">Home</Link>
+                        <Link to="/faq" className="navbar-item r-item">Features</Link>
+                        <Link to="/faq" className="navbar-item r-item">About</Link>
+                        <Link to="/faq" className="navbar-item r-item">FAQ</Link>
+                    
+                        <div className="navbar-item">
+                            <p className="control">
+                                <a className="button is-primary is-outlined">
+                                    <span className="icon">
+                                        <i className="fa fa-download"></i>
+                                    </span>
+                                    <span>Join Now</span>
+                                </a>
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
